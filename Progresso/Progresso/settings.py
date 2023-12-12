0a3b716f5ffc3009cae258c9aa19cdd397852bbf
alffy007 <jamesbond006aj@gmail.com>
2023-12-12 23:57:14 +0530

    update

--- Progresso/Progresso/settings.py
+++ Progresso/Progresso/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-IN'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'IST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
